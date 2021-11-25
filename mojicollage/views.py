97222c9d257be6b569d58f5cfba943d142385834
df.sol.cf@gmail.com <df.sol.cf@gmail.com>
2021-11-26 08:55:22 +0900

    少し回収

--- mojicollage/views.py
+++ mojicollage/views.py
@@ -4,11 +4,15 @@ from .forms import createInfo
 # Create your views here.
 
 def home(request):
-    initial_dict = {
-        'tgtSeikaku':'3',
-        'tgtKeigo':'0',
-        'finish':'0',
-    }
-    form = createInfo(request.POST or None, initial=initial_dict)
-    return render(request, 'mojicollage/home.html', {'form': form})
+    if request.method == "POST":
+        form = createInfo(request.POST, request.FILES)
+        return render(request, 'mojicollage/home.html', {'form': form})
+    else:
+        initial_dict = {
+            'tgtSeikaku':'3',
+            'tgtKeigo':'0',
+            'finish':'0',
+        }
+        form = createInfo(request.POST or None, initial=initial_dict)
+        return render(request, 'mojicollage/home.html', {'form': form})
 
